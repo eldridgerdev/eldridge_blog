e27006ad54d2786978fda72ee4198ba7089ab89b
Ryan Eldridge <eldridgerdev@gmail.com>
2020-11-26 09:33:50 -0600

    Fix Cloudinary Images

--- src/pages/blog-post-list.tsx
+++ src/pages/blog-post-list.tsx
@@ -11,13 +11,52 @@ const Inner = styled.div`
     width: 75%;
     
 `
+interface BlogListPageProps extends PageProps {
+  data: {
+    strapiBlogListPage: any
+  }
+}
 
-const BlogPostsPage: any = ({ data, location }) => {
+export const pageQuery = graphql`
+  query getBlogListPAge {
+    strapiBlogListPage {
+      siteTitle
+      heroText
+      heroImage {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+// Page {
+//   SiteTitle
+//   HeroText
+//   HeroImage {
+//     childImageSharp {
+//       fluid {
+//         ...GatsbyImageSharpFluid
+//       }
+//     }
+//   }
+// }
+const BlogPostsPage: React.FC<BlogListPageProps> = ({ data, location }) => {
 //   const siteTitle = data.site.siteMetadata.title
+  // const blogListData = data.strapiBlogListPage
+  // const image = blogListData.Page.HeroImage?.childImageSharp.fluid
+  // const { SiteTitle: title, HeroText: text } = blogListData.Page
+  const blogListData = data.strapiBlogListPage
+  const image = blogListData.heroImage?.childImageSharp.fluid
+  const { siteTitle: title, heroText: text } = blogListData
+
 
   return (
-    <Layout /*location={location} title={siteTitle} */>
-      <SEO title="Blog" />
+    <Layout heroOverride={image} heroText={text || null} /*location={location} title={siteTitle} */>
+      <SEO title={title} />
       {/* <h1>Coming Soon</h1>
       <p>This page does not exist yet, check back later</p> */}
       <Inner>
