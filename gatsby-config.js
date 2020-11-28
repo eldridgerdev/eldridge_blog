0e5ddf0b645387057032dad83833bb28db18b30b
Ryan Eldridge <eldridgerdev@gmail.com>
2020-11-28 12:00:23 -0600

    Fix blog post titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         fonts: [
           `opensans\:400,600,700`,
           `alegreya\:400,700`,
-          `raleway\:400,700`,
+          `raleway\:400,400i,700,700i`,
           `ubuntu mono\:400,700,800`,
           `lato\:400,700`
         ],
