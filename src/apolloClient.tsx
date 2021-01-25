791df28303bb70f7e28c0c9ce4185035f52d098b
Ryan Eldridge <eldridgerdev@gmail.com>
2021-01-24 17:55:30 -0800

    afsdgasdfghsdfg

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -2,10 +2,13 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import fetch from 'cross-fetch'
 
 console.log(process.env.REACT_APP_BACKEND_URL);
+console.log(process.env.GATSBY_REACT_APP_BACKEND_URL);
+console.log(process.env.GATSBY_API_URL);
+console.log(process.env.API_URL);
 
 const client = new ApolloClient({
     link: new HttpLink({
-        uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+        uri: `${process.env.GATSBY_API_URL}/graphql`,
         fetch,
         credentials: 'same-origin'
     }),
