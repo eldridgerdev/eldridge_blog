72b58a3c5307427bb29a3fb15b81ed4290bf562e
Ryan Eldridge <eldridgerdev@gmail.com>
2021-01-24 19:28:38 -0800

    add console

--- gatsby-node.js
+++ gatsby-node.js
@@ -135,7 +135,9 @@ exports.createResolvers = ({
     [name]: {
       type: 'File',
       resolve(source, args, context, info) {
+        // Uncomment for debuggint
         console.log(`---------------------------`);
+        console.log(`SOURCE: ${JSON.stringify(source)}`, null, 4);
         console.log(`URL: ${source.url}`);
         console.log(`NAME URL: ${source[name].url}`)
         return createRemoteFileNode({
