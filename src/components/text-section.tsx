e17892f9544f28a86500afc5924a752a2f7cc435
eldridgerdev <eldridgerdev@gmail.com>
2021-07-01 21:46:05 -0700

    Add dynamic zones for better control of blog posts and images

--- src/components/text-section.tsx
+++ src/components/text-section.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro'
+import Image, { FluidObject } from 'gatsby-image'
 
 import Line from './line/line'
 import Text from './text'
 import theme from './../utils/theme'
+import {
+  BlogContent,
+  BlogContentItem,
+  StrapiComponent,
+  ImageWidthOptions,
+} from '../hooks/use-all-posts'
+
+interface SectionProps {
+  title: string
+  content: BlogContent
+}
+
+const TextSection = styled(Text)`
+  width: 100%;
+  ${tw`flex-auto`}
+`
 
 const SectionLine = styled(Line)`
   border-top: 2px solid ${theme.colors.main};
@@ -19,17 +36,149 @@ const Container = styled.div`
   ${tw`w-full md:w-1/2`}
 `
 
-const Section: React.FC<{ text: string; title: string }> = ({
-  text,
-  title,
-}) => (
+const getWidth = (width?: ImageWidthOptions) => {
+  switch (width) {
+    case ImageWidthOptions.SMALL: {
+      return '50%'
+    }
+    case ImageWidthOptions.MEDIUM: {
+      return '75%'
+    }
+    case ImageWidthOptions.LARGE: {
+      return '100%'
+    }
+    default: {
+      return '50%'
+    }
+  }
+}
+const ImageContainer = styled.div<{ $width?: ImageWidthOptions }>`
+  width: ${(props: { $width?: ImageWidthOptions }) => getWidth(props.$width)};
+  ${tw`flex-auto min-w-0 mx-auto my-3`}
+`
+
+//@TODO: Move to it's own file
+const AffiliateLinkContainer = styled.div`
+  background-color: ${theme.colors.light};
+  & ul {
+    ${tw`flex flex-wrap justify-evenly w-full`}
+  }
+  & li {
+    ${tw`flex`}
+  }
+
+  & p {
+    ${tw`text-sm md:text-sm text-center`}
+  }
+
+  ${tw`flex-auto min-w-0 p-2 my-3`}
+`
+
+const AffiliateLinkRow = styled.div`
+  ${tw`flex justify-center my-1`}
+`
+
+const BlogTextSection = styled(Text)`
+  ${tw`my-3`}
+`
+
+type AffiliateLinkProps = {
+  linkText: string
+}
+
+const AffiliateLinks = ({ linkText }: AffiliateLinkProps) => (
+  <AffiliateLinkContainer>
+    <AffiliateLinkRow>
+      <Text>
+        <p>
+          <i>
+            We earn a small commision if you purchase a recommended product
+            through our affiliate link
+          </i>
+        </p>
+      </Text>
+    </AffiliateLinkRow>
+    <AffiliateLinkRow dangerouslySetInnerHTML={{ __html: linkText }} />
+  </AffiliateLinkContainer>
+)
+
+type ImageProps = {
+  image: FluidObject
+  caption?: string
+  width?: ImageWidthOptions
+}
+
+// @TODO: Probably deserves it's own file.
+const renderImage = ({ image, caption, width }: ImageProps) => (
+  <ImageContainer $width={width}>
+    <br />
+    <figure className="image image_resized image-style-align-center">
+      <Image
+        fluid={image}
+        imgStyle={{
+          objectPosition: 'left center',
+        }}></Image>
+      <figcaption>{caption}</figcaption>
+    </figure>
+    <br />
+  </ImageContainer>
+)
+
+const renderText = (content: BlogContent) => {
+  return (
+    <div>
+      {content &&
+        content.map((item, i) => {
+          switch (item.strapi_component) {
+            case StrapiComponent.IMAGE: {
+              if (!item.Image || !item.Image.childImageSharp) {
+                return
+              }
+
+              const width = item.ImageWidth || ImageWidthOptions.SMALL
+
+              return (
+                <Text key={i}>
+                  {renderImage({
+                    image: item.Image.childImageSharp.fluid,
+                    caption: item.ImageCaption,
+                    width: width,
+                  })}
+                </Text>
+              )
+            }
+            case StrapiComponent.AFFILIATE: {
+              if (!item.AffiliateLinkText) {
+                return
+              }
+
+              return (
+                <AffiliateLinks key={i} linkText={item.AffiliateLinkText} />
+              )
+            }
+            default: {
+              if (!item.Text) {
+                return
+              }
+              return (
+                <BlogTextSection key={i}>
+                  <div dangerouslySetInnerHTML={{ __html: item.Text }} />
+                </BlogTextSection>
+              )
+            }
+          }
+        })}
+      <br />
+    </div>
+  )
+}
+
+const Section: React.FC<SectionProps> = ({ title, content }) => (
   <Container>
     <Text>
       <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: text }} />
     </Text>
-
-    <br />
+    {renderText(content)}
     <SectionLine />
     {/* <Text dangerouslySetInnerHTML={{ __html: text }} /> */}
   </Container>
