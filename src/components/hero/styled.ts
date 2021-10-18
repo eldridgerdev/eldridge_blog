34d5c7f5119cd7f5cc38c5ee24f0e46224383f14
eldridgerdev <eldridgerdev@gmail.com>
2021-10-18 11:39:00 -0700

    actually fix the hero image

--- src/components/hero/styled.ts
+++ src/components/hero/styled.ts
@@ -36,27 +36,26 @@ export const HeroText = styled.div`
 
 export const CustomImage = styled(Image)<CustomImageProps>`
   max-height: 100%;
-  position: initial
-    ${(props: CustomImageProps) => {
-      let propStyles = ``
-      if (props.height) {
-        propStyles = `
+  ${(props: CustomImageProps) => {
+    let propStyles = ``
+    if (props.height) {
+      propStyles = `
           ${propStyles}
           height: ${props.height};
         `
-      }
+    }
 
-      if (props.mobileHeight) {
-        propStyles = `
+    if (props.mobileHeight) {
+      propStyles = `
           ${propStyles}
           @media screen and (max-width: 600px) {
             height: ${props.mobileHeight};
           }
         `
-      }
+    }
 
-      return propStyles
-    }};
+    return propStyles
+  }};
 `
 
 // Has to be an object instead of normal styled components for gatsby-image's "imgStyle" prop
